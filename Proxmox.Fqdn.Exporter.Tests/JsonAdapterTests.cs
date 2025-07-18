f5e64750c761d6e0eec2a0709433f20fe995010a
Jonathan GUICHARD <jonathan.guichard@coexya.eu>
2025-07-18 03:41:56 +0200

    Feature -- Add unitary tests

---
+++ Proxmox.Fqdn.Exporter.Tests/JsonAdapterTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging.Abstractions;
+using Proxmox.Fqdn.Exporter.Adapters;
+using Proxmox.Fqdn.Exporter.Data;
+
+namespace Proxmox.Fqdn.Exporter.Tests;
+
+public class JsonAdapterTests
+{
+	[Fact]
+	public async Task ParseNetworkGetInterfacesResult()
+	{
+		var data = await File.ReadAllTextAsync("data.json");
+
+		var sut = new JsonAdapter(new NullLogger<JsonAdapter>());
+
+		var result = sut.ParseIot<NetworkGetInterfacesResult[]>(data, NetworkGetInterfacesContext.Default);
+
+		Assert.NotEmpty(result);
+	}
+}
